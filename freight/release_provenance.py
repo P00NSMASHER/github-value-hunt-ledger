b078ae4b83bda3558c8a3872943aa3778ad2f230
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-20 21:13:23 -0400

    Bind external action authorization into Freight provenance

--- freight/release_provenance.py
+++ freight/release_provenance.py
@@ -25,6 +25,7 @@ CONTROL_PATHS = (
     "freight/deal_economics.py",
     "freight/deployment_security_evidence.py",
     "freight/engagement_state.py",
+    "freight/external_action_authorization.py",
     "freight/gap_registry.py",
     "freight/input_guard.py",
     "freight/incident_response.py",
