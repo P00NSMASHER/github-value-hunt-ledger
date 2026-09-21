9714b39af183a69c0cde402338b412d3763f52ab
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-20 20:52:58 -0400

    Include engagement state resolver documentation in diligence bundle

--- freight/diligence_bundle.py
+++ freight/diligence_bundle.py
@@ -30,6 +30,7 @@ DOC_PATHS=(
     "freight/PILOT_ACTIVATION_PACKET.md",
     "freight/PILOT_CHARTER.md",
     "freight/PILOT_AMENDMENT.md",
+    "freight/ENGAGEMENT_STATE.md",
     "freight/SEPARATE_ENVIRONMENT_EVIDENCE.md",
     "freight/SEPARATE_ENVIRONMENT_EVIDENCE_TEMPLATE.json",
     "freight/PILOT_DATA_ROOM.md",
