ff50a6bf875db7455f0e48839522e5c25b8965cd
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-22 08:40:13 -0400

    RecoveryWorks: export review queue API

--- recoveryworks/__init__.py
+++ recoveryworks/__init__.py
@@ -4,6 +4,7 @@ from .fees import FeeAgreement, FeeAssessment, assess_fee, calculate_fee_cents
 from .ledger import LedgerEvent, LedgerRecord, RecoveryLedger
 from .packets import RecoveryPacket, build_client_portfolio_packet, build_recovery_packet, submission_ready
 from .raw_scan import build_raw_scan, execute_raw_scan_payload, run_raw_scan_payload
+from .review import ReviewQueueItem, build_review_queue, review_queue_summary
 from .scan import RecoveryScanBatch, RecoveryScanManifest, SourceManifestEntry, freeze_scan, run_scan
 from .storage import export_ledger, import_ledger, load_ledger, save_ledger
 from .models import (
@@ -38,6 +39,9 @@ __all__ = [
     "RuleRef",
     "SourceManifestEntry",
     "build_client_portfolio_packet",
+    "ReviewQueueItem",
+    "build_review_queue",
+    "review_queue_summary",
     "build_raw_scan",
     "execute_raw_scan_payload",
     "run_raw_scan_payload",
