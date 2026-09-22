0d075df99df24137ee29fbc53b4f42287f4e9987
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-22 09:19:56 -0400

    Export UtilityRecovery branch adapters

--- recoveryworks/branches/__init__.py
+++ recoveryworks/branches/__init__.py
@@ -2,6 +2,23 @@ from .ap import APObligation, APPayment, build_ap_observations, normalize_invoic
 from .ap_csv import load_obligations_csv, load_payments_csv, money_to_cents
 from .base import BranchInput, RuleBackedAdapter
 from .registry import BRANCHES, BranchSpec, all_specs
+from .utility import (
+    UtilityAuditBatch,
+    UtilityAuditException,
+    UtilityBill,
+    UtilityCharge,
+    UtilityChargeKind,
+    UtilityTariff,
+    audit_utility_bills,
+    calculate_expected_bill,
+    normalize_service_class,
+)
+from .utility_io import (
+    dollars_per_unit_to_micros,
+    dollars_to_cents,
+    load_simple_tariff_definitions_json,
+    load_utility_bills_csv,
+)
 
 __all__ = [
     "APObligation",
@@ -10,10 +27,23 @@ __all__ = [
     "BranchInput",
     "BranchSpec",
     "RuleBackedAdapter",
+    "UtilityAuditBatch",
+    "UtilityAuditException",
+    "UtilityBill",
+    "UtilityCharge",
+    "UtilityChargeKind",
+    "UtilityTariff",
     "all_specs",
+    "audit_utility_bills",
     "build_ap_observations",
+    "calculate_expected_bill",
+    "dollars_per_unit_to_micros",
+    "dollars_to_cents",
     "load_obligations_csv",
     "load_payments_csv",
+    "load_simple_tariff_definitions_json",
+    "load_utility_bills_csv",
     "money_to_cents",
     "normalize_invoice_number",
+    "normalize_service_class",
 ]
