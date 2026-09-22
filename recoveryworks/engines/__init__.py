0def5e605f9cb32de0a05ed3184d65d5f8f4542d
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-22 08:20:23 -0400

    RecoveryWorks: export payer engine

--- recoveryworks/engines/__init__.py
+++ recoveryworks/engines/__init__.py
@@ -1,5 +1,6 @@
 """Deterministic branch engines owned by RecoveryWorks."""
 from .ap import APInvoice, APPayment, detect_ap_overpayments
+from .payer import FeeScheduleRate, PayerServiceLine, detect_payer_underpayments
 from .utility import (
     EnergyTier,
     UtilityBill,
@@ -16,12 +17,15 @@ __all__ = [
     "APInvoice",
     "APPayment",
     "EnergyTier",
+    "FeeScheduleRate",
+    "PayerServiceLine",
     "UtilityBill",
     "UtilityCalculation",
     "UtilityTariff",
     "bill",
     "calculate_expected_bill",
     "detect_ap_overpayments",
+    "detect_payer_underpayments",
     "detect_utility_variance",
     "tariff",
     "tier",
