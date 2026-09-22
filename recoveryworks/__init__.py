fcc22bf18efbd5e0af89406e358598f3dea58e12
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-22 09:00:34 -0400

    Export durable RecoveryWorks ledger APIs

--- recoveryworks/__init__.py
+++ recoveryworks/__init__.py
@@ -1,5 +1,7 @@
 """RecoveryWorks / RecoveryOS shared recovery intelligence layer."""
+from .durable_ledger import DurableRecoveryLedger
 from .engine import RecoveryEngine, RecoveryObservation
+from .journal import JournalEvent, RecoveryJournal
 from .ledger import RecoveryLedger
 from .scan import RecoveryScanBatch, RecoveryScanManifest, SourceManifestEntry, freeze_scan, run_scan
 from .models import (
@@ -15,10 +17,13 @@ from .models import (
 __all__ = [
     "Branch",
     "CaseState",
+    "DurableRecoveryLedger",
     "EvidenceRef",
     "FindingState",
+    "JournalEvent",
     "RecoveryEngine",
     "RecoveryFinding",
+    "RecoveryJournal",
     "RecoveryLedger",
     "RecoveryMode",
     "RecoveryObservation",
