347b4c76ae3f607d86b764a2b7b6ae20753d5966
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-22 09:24:56 -0400

    ci: validate measurement plan against learning curriculum

--- tools/ti_build.py
+++ tools/ti_build.py
@@ -52,6 +52,7 @@ STEPS = (
     ['tools/ti_adjacency.py'],
     ['tools/ti_adjacency_validate.py'],
     ['tools/ti_measurement_plan.py'],
+    ['tools/ti_measurement_plan_validate.py'],
     ['tools/ti_measurement_campaign.py'],
     ['tools/ti_surface.py'],
     ['tools/ti_allocator_learning.py'],
