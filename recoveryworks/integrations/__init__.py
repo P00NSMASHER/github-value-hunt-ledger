040ecda6ce31d1e30262cb626126f2e7e6ffcc8f
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-23 23:36:47 -0400

    Export Cletrics savings signal role

--- recoveryworks/integrations/__init__.py
+++ recoveryworks/integrations/__init__.py
@@ -8,6 +8,7 @@ from .cletrics import (
     load_cletrics_bundle,
     ANOMALY_ROLE,
     RECONCILIATION_ROLE,
+    SAVINGS_ROLE,
 )
 
 __all__ = [
@@ -18,4 +19,5 @@ __all__ = [
     "load_cletrics_bundle",
     "ANOMALY_ROLE",
     "RECONCILIATION_ROLE",
+    "SAVINGS_ROLE",
 ]
