833942defc2adc2f2c1c899de1ba5176fe9f1f00
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-24 08:29:52 -0400

    MNPI ingest step 8: expose review gate API

--- historical_mnpi/__init__.py
+++ historical_mnpi/__init__.py
@@ -55,6 +55,21 @@ from .extractors import (
     extract_sec_pdf_page_text_candidates,
 )
 
+from .review_queue import (
+    HistoricalReviewDecision,
+    HistoricalReviewQueue,
+    ReviewChecks,
+    ReviewConflict,
+    ReviewDecision,
+    ReviewIdentitySnapshot,
+    ReviewQueueItem,
+    ReviewTemporalSnapshot,
+    build_review_item,
+    decide_review_item,
+    detect_candidate_conflicts,
+    render_review_item_markdown,
+)
+
 from .source_registry import (
     SourceAdmissibility,
     SourceRecord,
@@ -65,6 +80,18 @@ from .source_registry import (
 )
 
 __all__ = [
+    "HistoricalReviewDecision",
+    "HistoricalReviewQueue",
+    "ReviewChecks",
+    "ReviewConflict",
+    "ReviewDecision",
+    "ReviewIdentitySnapshot",
+    "ReviewQueueItem",
+    "ReviewTemporalSnapshot",
+    "build_review_item",
+    "decide_review_item",
+    "detect_candidate_conflicts",
+    "render_review_item_markdown",
     "CandidateField",
     "CandidateFieldStatus",
     "CandidateKind",
