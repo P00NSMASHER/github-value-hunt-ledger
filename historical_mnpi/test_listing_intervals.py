b21acafd8d47164cb2e917110cae3f72a207e6f7
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-25 18:59:21 -0400

    MNPI G2 batch 23: finish 79-interval test update

--- historical_mnpi/test_listing_intervals.py
+++ historical_mnpi/test_listing_intervals.py
@@ -328,7 +328,7 @@ class ListingIntervalTests(unittest.TestCase):
                 encoding="utf-8"
             )
         )
-        self.assertEqual(len(intervals), 74)
+        self.assertEqual(len(intervals), 79)
         for item in intervals:
             self.assertTrue(item.start_evidence_url.startswith(
                 "https://www.sec.gov/"
