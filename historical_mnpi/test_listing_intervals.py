2e85144806043d05161e8ca141eb36c5ba161ebe
P00NSMASHER <jamisonperkins@yahoo.com>
2026-09-25 16:51:36 -0400

    MNPI G2 batch 14: fix SEC interval-count assertion

--- historical_mnpi/test_listing_intervals.py
+++ historical_mnpi/test_listing_intervals.py
@@ -297,7 +297,7 @@ class ListingIntervalTests(unittest.TestCase):
                 encoding="utf-8"
             )
         )
-        self.assertEqual(len(intervals), 45)
+        self.assertEqual(len(intervals), 48)
         for item in intervals:
             self.assertTrue(item.start_evidence_url.startswith(
                 "https://www.sec.gov/"
